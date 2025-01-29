ecc3fc0ad85d9f0a5605f0701a21c002d29440ec
Alexsandro-Simas-Lopes <alexsimaslopes@gmail.com>
2025-01-29 16:57:46 -0400

    DELETE criado e em funcionamento

--- produtos/control/produto_remover.php
+++ produtos/control/produto_remover.php
@@ -1,5 +1,5 @@
 <?php
-require_once("../../../produto/produto/model/produtodao.php");
+require_once("../../produtos/model/produtodao.php");
 class produto_remover
 {
     public function __construct()
@@ -16,17 +16,7 @@ class produto_remover
             } else {
                 $error_message = "Não é possível excluir o produto";
                 if ($exec->getCode() == 23000) {
-                    if (strpos($exec->getMessage(), 'y_produto_plataforma_parametro') !== false) {
-                        $error_message = "Não é possível excluir o produto <br><strong>(#_txt_#)</strong> <br> porque ele está associado a parâmetro.";
-                    } else  if (strpos($exec->getMessage(), 'y_produto_identificacao_assinatura') !== false) {
-                        $error_message = "Não é possível excluir o produto <br><strong>(#_txt_#)</strong> <br> porque ele está associado a identificação.";
-                    } else  if (strpos($exec->getMessage(), 'y_produto_identificacao') !== false) {
-                        $error_message = "Não é possível excluir o produto <br><strong>(#_txt_#)</strong> <br> porque ele está associado a identificação.";
-                    } else  if (strpos($exec->getMessage(), 'voucher') !== false) {
-                        $error_message = "Não é possível excluir o produto <br><strong>(#_txt_#)</strong> <br> porque ele está associado a voucher.";
-                    } else  if (strpos($exec->getMessage(), 'assinatura') !== false) {
-                        $error_message = "Não é possível excluir o produto <br><strong>(#_txt_#)</strong> <br> porque ele está associado a assinatura.";
-                    }
+                    echo $error_message;
                 } else {
                     $error_message = "Houve um erro (tente novamente)";
                 }
